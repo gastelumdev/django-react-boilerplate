a6ee80c17facd16f0ea0af38cbe99388791bef0d
Omar Gastelum <ogastelum@collabtime.co>
2023-01-02 05:45:27 -0800

    Add auth to leads

--- project/frontend/urls.py
+++ project/frontend/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('favicon.ico', RedirectView.as_view(
+        url=staticfiles_storage.url('/favicon.ico')))
 ]
