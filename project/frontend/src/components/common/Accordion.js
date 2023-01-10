a3ec0a77fed746fdffd2cdb19e57b34e2145ffe4
Omar Gastelum <ogastelum@collabtime.co>
2023-01-10 08:01:15 -0800

    Fix accordion

--- project/frontend/src/components/common/Accordion.js
+++ project/frontend/src/components/common/Accordion.js
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Accordion = (props) => {
+    const [show, setShow] = useState(false);
+
     return (
-        <div className=" mb-5 mt-5" id="accordion">
+        <div className=" mb-5 mt-4" id="accordion">
             <div className="card">
                 <div className="card-header" id="headingOne">
                     <h5 className="mb-0">
-                        <button className="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        <button className="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" onClick={() => setShow(!show)}>
                             {props.title}
                         </button>
                     </h5>
                 </div>
 
-                <div id="collapseOne" className={`collapse ${props.show}`} aria-labelledby="headingOne" data-parent="#accordion">
+                <div id="collapseOne" className={`collapse ${show ? 'show' : ''}`} aria-labelledby="headingOne" data-parent="#accordion">
                     <div className="card-body">
                         {props.children}
                     </div>
